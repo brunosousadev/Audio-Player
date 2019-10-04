71e1c1dbe440f9e8add11c55a698c858a51b57d9
Bruno Sousa <brse01@gmail.com>
2019-10-04 19:41:34 -0300

    Creating Audio Player with Javascript

---
+++ playerElements.js
@@ -0,0 +1,31 @@
+import {secondsToMinutes} from './utils.js';
+
+export default {
+    get(){
+        this.cover  = document.querySelector('.card-image');
+        this.title  = document.querySelector('.card-content h5');
+        this.artist = document.querySelector('.artist');
+        this.playPause = document.querySelector('#play-pause')
+        this.mute = document.querySelector('#mute');
+        this.volume = document.querySelector('#vol-control');
+        this.seekBar = document.querySelector('#seek_bar');
+        this.currentDuration =  document.querySelector('#current-duration');
+        this.totalDuration   =   document.querySelector('#total-duration');
+                
+    },
+    createAudioElement(audio){
+        this.audio = new Audio(audio);        
+    },
+    actions(){
+        this.audio.onended = () => this.next();
+        this.audio.ontimeupdate = () => this.timeUpdate();
+        this.playPause.onclick = () =>  this.togglePlayPause();
+        this.mute.onclick = () => this.toggleMute();
+        this.volume.oniput = ()=> this.setVolume(this.volume.value);
+        this.volume.onchange = ()=> this.setVolume(this.volume.value);
+        this.seekBar.oniput = ()=> this.setSeek(this.seekBar.value);
+        this.seekBar.onchange = ()=> this.setSeek(this.seekBar.value);    
+        this.seekBar.max = this.audio.duration;
+        this.totalDuration.innerText =secondsToMinutes( this.audio.duration);        
+    }
+}
